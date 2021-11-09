fa9f3c0d091ec676ef89ec56c0ff8147df95e4d2
Cole Brooks <cole.brooks@hotmail.com>
2021-11-08 18:33:30 -0600

    added the calendar and a dummy event.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,49 @@
+// default imports - came from gatsby
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+// Calendar import stuff - learned from video BUILD A REACT JS CALENDAR APP - Darwin Tech
+import { Calendar, dateFnsLocalizer } from "react-big-calendar"; 
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US")
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
+
+const events = [
+  {
+    id: 0,
+    title: "Dummy Poll",
+    allDay: false,
+    start: new Date(2021,10,8,0,0), // not sure why but the month is indexed from zero?
+    end: new Date(2021,10,8,10,0)
+  }
+]
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+
+    <div className = "App">
+      <Calendar localizer={localizer} events = {events} startAccessor={"start"} endAccessor={"end"} style = {{height:750, margin: "25px"}} />
+    </div>
+
   </Layout>
 )
 
