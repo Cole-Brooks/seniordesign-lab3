9382f7255503790bb0d34599c8ffcdbeb9f9c6ad
Cole Brooks <55246154+Cole-Brooks@users.noreply.github.com>
2021-11-10 18:39:27 -0600

    Revert "Finished Initial Design of Website"

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,26 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-// Navbar imports
-import ReactDOM from 'react-dom'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Redirect,
-  Switch
-} from 'react-router-dom'
-import Navbar from "../components/Navbar"
+// Calendar import stuff - learned from video BUILD A REACT JS CALENDAR APP - Darwin Tech
+import { Calendar, dateFnsLocalizer } from "react-big-calendar"; 
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
 
-// Pages
-import CashCalendar from "./CashCalendar"
-import CreateEvent from "./CreateEvent"
-import Polls from "./Polls"
-import CreatePoll from "./CreatePoll"
+const locales = {
+  "en-US": require("date-fns/locale/en-US")
+}
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
 
 const events = [
   {
@@ -37,16 +39,11 @@ const events = [
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <Router>
-      <main>
-        <Routes>
-          <Route path="/" exact element={<CashCalendar/>} />
-          <Route path="/CreateEvent" exact element={<CreateEvent />} />
-          <Route path="/Polls" exact element={<Polls/>} />
-          <Route path="/CreatePoll" exact element={<CreatePoll/>} />
-        </Routes>
-      </main>
-    </Router>
+
+    <div className = "App">
+      <Calendar localizer={localizer} events = {events} startAccessor={"start"} endAccessor={"end"} style = {{height:750, margin: "25px"}} />
+    </div>
+
   </Layout>
 )
 
