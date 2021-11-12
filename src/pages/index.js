22765fe6af58548f1e80c11b507276a1d26d1914
Cole Brooks <cole.brooks@hotmail.com>
2021-11-12 09:51:26 -0600

    finished database functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,19 +11,6 @@ import Navbar from "../components/Navbar"
 
 const IndexPage = () => 
 {
-  const ranDay = new Date(2021, 10, Math.floor(Math.random() * (30-1 + 1) + 1))
-
-  const attendees = [
-    {
-      name: "Random Person",
-      email: "random.person.email@email.com"
-    },
-    {
-      name: "Random Person2",
-      email: "random.person2.email@email.com"
-    }
-  ]
-
   return (
   <Layout>
     <Seo title="Home" />
