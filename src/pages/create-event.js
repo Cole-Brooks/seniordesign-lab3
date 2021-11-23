b2af8ebf207d75a237eb331f3e5e586202692b1f
Thomas, Ann V <ann-thomas@uiowa.edu>
2021-11-23 14:57:48 -0600

    form complete

--- src/pages/create-event.js
+++ src/pages/create-event.js
@@ -7,10 +7,19 @@ import Navbar from "../components/Navbar"
 const CreateEvent = () => {
     return (
         <Layout>
-            <Seo title="CreateEvent" />
+                <Seo title="CreateEvent" />
             <Navbar />
             <main>
                 <h1> Create Event </h1>
+                <form action="/action_page.php">
+                    <label for="eventTitle">Title:</label>
+                    <input type="text" id="eventTitle" name="eventTitle"></input><br></br>
+                    <label for="eventDate">Date:</label>
+                    <input type="date" id="eventDate" name="eventDate"></input><br></br>
+                    <label for="eventTime">Time:</label>
+                    <input type="time" id="eventTime" name="eventTime"></input><br></br>
+                    <input type="submit"></input>
+                </form>
             </main>
         </Layout>
     )
