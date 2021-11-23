afe2eb26ce1da7dbfc342b38e02c92020c0b2707
Thomas, Ann V <ann-thomas@uiowa.edu>
2021-11-23 16:24:25 -0600

    form added, making submit button work

--- src/pages/create-event.js
+++ src/pages/create-event.js
@@ -4,25 +4,82 @@ import Seo from "../components/seo"
 
 import Navbar from "../components/Navbar"
 
-const CreateEvent = () => {
-    return (
-        <Layout>
+class CreateEvent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // this.state = {value: ''};
+    
+        // this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            eventTitle:"",
+            eventDate:"",
+            eventTime:""
+        }
+      }
+
+
+     handleSubmit(item,type){
+        
+        // var jFirst = document.getElementById("eventTitle").value;
+        // var jLast = document.getElementById("eventDate").value;
+        // alert(this.state.value);
+        // e.preventDefault();
+        
+        let itemValue = item.target.value;
+        switch(type){
+            case "eventTitle" :{
+                this.setState({eventTitle: itemValue}); 
+            }
+            case "eventDate" :{
+                this.setState({eventDate: itemValue}); 
+            }
+            case "eventTime" :{
+                this.setState({eventTime: itemValue}); 
+            }
+           
+        }
+        console.log("all",this.state)
+     }
+
+     submit(){
+         let obj = {};
+         obj.eventTitle = this.state.eventTitle;
+         obj.eventDate = this.state.eventDate;
+         obj.eventTime = this.state.eventTime;
+         console.log("submit data", obj);
+
+     }
+
+    //  handleChange(e) {
+    //     this.setState({value: e.target.value});
+    //   }
+
+    render(){
+        return (
+            <Layout>
+    
                 <Seo title="CreateEvent" />
-            <Navbar />
-            <main>
-                <h1> Create Event </h1>
-                <form action="/action_page.php">
-                    <label for="eventTitle">Title:</label>
-                    <input type="text" id="eventTitle" name="eventTitle"></input><br></br>
-                    <label for="eventDate">Date:</label>
-                    <input type="date" id="eventDate" name="eventDate"></input><br></br>
-                    <label for="eventTime">Time:</label>
-                    <input type="time" id="eventTime" name="eventTime"></input><br></br>
-                    <input type="submit"></input>
-                </form>
-            </main>
-        </Layout>
-    )
+                <Navbar />
+                <main>
+                    <h1> Create Event </h1>
+                
+                    <input type="text" placeHolder="Event Title" onChange={(item)=>this.handleSubmit(item,"eventTitle")}></input><br></br>
+                    
+                    <input type="date" placeHolder="Event Date" onChange={(item)=>this.handleSubmit(item,"eventDate")}></input><br></br>
+                    
+                    <input type="time" placeHolder="Event Time" onChange={(item)=>this.handleSubmit(item,"eventTime")}></input><br></br>
+                
+
+                    <button onClick={()=>this.submit()}> Submit </button>
+                    
+                </main>
+            </Layout>
+        );
+    }
+    
 }
 
 export default CreateEvent
