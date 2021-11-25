e9598a8cb022fa7a77fabaab26bc5e2047e46c3a
Steve Wasiswa <steve-wasiswa@uiowa.edu>
2021-11-25 11:22:04 -0600

    Updated routes in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,36 @@
-import React from 'react'
-import { Link, navigate } from "gatsby"
+import React, {useContext} from "react"
+import { Link } from "gatsby"
+import {AuthContext} from "../context/auth"
 
 
 const Navbar = () => {
+    const { user } = useContext(AuthContext)
     return (
         <div>
            <nav>
-              <Link style={{ textDecoration: 'none' }} to="/">Calendar </Link>
-              {` `}
-              <Link style={{ textDecoration: 'none' }} to="/create-event">Create Event </Link>
-              {` `}
-              <Link style={{ textDecoration: 'none' }} to="/polls">Polls </Link>
-              {` `}
-              <Link style={{ textDecoration: 'none' }} to="/create-poll">Create Poll</Link>
-              {` `}
-           </nav>
+           { !user ?
+              <div style={{alignItems:"center", display:"flex"}}>
+                <p>
+                   <Link style={{ textDecoration: 'none' }} to="/">Calendar </Link>
+                </p>
+              </div>
+           :(
+             <div style={{alignItems:"center", display:"flex"}}>
+                <p>
+                   <Link style={{ textDecoration: 'none' }} to="/">Calendar </Link>
+                </p>
+                <p>
+                   <Link style={{ textDecoration: 'none' }} to="/create-event">Create Event </Link>
+                </p>
+                <p>
+                   <Link style={{ textDecoration: 'none' }} to="/polls">Polls </Link>
+                </p>
+                <p>
+                   <Link style={{ textDecoration: 'none' }} to="/create-poll">Create Poll</Link>
+                </p>
+             </div>
+          )}
+          </nav>
         </div>
     )
 }
