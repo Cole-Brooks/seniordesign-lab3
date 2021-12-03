e9bcb0fd0ea03b8b038f41665df65e86cf5b800e
Steve Wasiswa <steve-wasiswa@uiowa.edu>
2021-12-03 06:05:11 -0600

    Added some styles to the registration form and change the flow

--- src/pages/register.js
+++ src/pages/register.js
@@ -4,6 +4,8 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import {AuthContext} from "../context/auth"
 import firebase from 'gatsby-plugin-firebase'
+import Navbar from "../components/Navbar";
+import CashCalendar from "../components/cash-calendar";
 
 const Register = () => {
     const [data, setData] = useState({
@@ -18,46 +20,80 @@ const Register = () => {
        setData({...data, [e.target.name]: e.target.value })
     }
 
-    const handleSubmit = async e => {
+    const handleRegistration = async e => {
        e.preventDefault()
        setData({...data, error: null})
        try{
            const result = await firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
            setUser(result)
-           navigate("/")
+           await navigate("/")
        }catch(err){
            setData({...data, error: err.message })
        }
     }
 
-    return (
-       <Layout>
-       <Seo title="Register" />
-        <h1>Sign Up</h1>
-        <form onSubmit={handleSubmit}>
-           <div>
-             <label htmlFor="email">Email</label>
-             <br/>
-             <input type="text" name="email" value={data.email} onChange={handleChange}/>
-             <br/>
-             <br/>
-           </div>
-           <div>
-             <label htmlFor="password">Password</label>
-             <br/>
-             <input type="password" name="password"  value={data.password} onChange={handleChange} />
-             <br/>
-             <br/>
-           </div>
-           {data.error ? <p style={{color:'red'}}>{data.error}</p> : null}
-            <div>
-             <br/>
-             <input type="submit" style={{color:'blue'}} value="Register"/>
-             <br/>
-           </div>
-        </form>
-        </Layout>
-    )
+    const user = firebase.auth().currentUser;
+
+    if (user) {
+        return(
+            <Layout>
+                <Seo title="Home" />
+                <Navbar />
+                <CashCalendar />
+            </Layout>
+        )
+    }else {
+
+        return (
+            <Layout>
+                <Seo title="Register"/>
+                <h1 style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center"
+                }}>Sign Up</h1>
+                <form onSubmit={handleRegistration}>
+                    <div>
+                        <label htmlFor="email" style={{
+                          marginLeft:'360px',
+                        }}>Email</label>
+                        <br/>
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                            }}>
+                            <input type="text" name="email" value={data.email} onChange={handleChange}/>
+                        </div>
+                        <br/>
+                    </div>
+                    <div>
+                        <label htmlFor="password" style={{
+                          marginLeft:'360px',
+                        }}>Password</label>
+                        <br/>
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                            }}>
+                             <input type="password" name="password" value={data.password} onChange={handleChange}/>
+                        </div>
+                        <br/>
+                    </div>
+                    {data.error ? <p style={{color: 'red'}}>{data.error}</p> : null}
+                    <div style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"}}>
+                        <br/>
+                        <input type="submit" style={{color: 'blue'}} value="Register"/>
+                        <br/>
+                    </div>
+                </form>
+            </Layout>
+        )
+    }
 }
 
 export default Register
