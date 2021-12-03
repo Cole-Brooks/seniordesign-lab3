a7b1c55a3172ac3051cab2d74f96cce2229cf592
Hongyu Zeng <zdszym@me.com>
2021-12-02 23:59:36 -0600

    nicer looking navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, {useContext} from "react"
-import { Link } from "gatsby"
+import { Link, navigate} from "gatsby"
 import {AuthContext} from "../context/auth"
+import { Stack, Button } from "@mui/material"
 
 
 const Navbar = () => {
@@ -9,14 +10,17 @@ const Navbar = () => {
         <div>
            <nav>
            { !user ?
-              <div style={{alignItems:"center", display:"flex"}}>
-                <p>
-                   <Link style={{ textDecoration: 'none' }} to="/">Calendar </Link>
-                </p>
-              </div>
+            //   <div style={{alignItems:"center", display:"flex"}}>
+            //     <p>
+            //        <Link style={{ textDecoration: 'none' }} to="/">Calendar </Link>
+            //     </p>
+            //   </div>
+               <Stack spacing={2} direction="row">
+                  <Button variant="outlined" onClick={() => navigate("/")}>Calendar</Button>
+               </Stack>
            :(
              <div style={{alignItems:"center", display:"flex"}}>
-                <p>
+                {/* <p>
                    <Link style={{ textDecoration: 'none' }} to="/">Calendar </Link>
                 </p>
                 <p>
@@ -27,7 +31,13 @@ const Navbar = () => {
                 </p>
                 <p>
                    <Link style={{ textDecoration: 'none' }} to="/create-poll">Create Poll</Link>
-                </p>
+                </p> */}
+                <Stack spacing={2} direction="row">
+                  <Button variant="outlined" onClick={() => navigate("/")}>Calendar</Button>
+                  <Button variant="outlined" onClick={() => navigate("/create-event")}>Create Event</Button>
+                  <Button variant="outlined" onClick={() => navigate("/polls")}>Polls</Button>
+                  <Button variant="outlined" onClick={() => navigate("/create-poll")}>Create Poll</Button>
+               </Stack>
              </div>
           )}
           </nav>
